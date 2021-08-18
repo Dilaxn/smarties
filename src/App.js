c1685ea0e4d9e70303bce52decfae708fc79fdf7
Dilaxn <dilaxn@gmail.com>
2021-08-18 11:14:17 +0530

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, {useContext, useEffect} from 'react';
+import {ThemeProvider} from '@material-ui/core';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import MainLayout from "./layouts/MainLayout";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        {/*<ThemeProvider >*/}
+          {/*<AuthProvider>*/}
+            <MainLayout>
+
+            </MainLayout>
+          {/*</AuthProvider>*/}
+        {/*</ThemeProvider>*/}
+      </BrowserRouter>
   );
 }
 
---
+++ src/layouts/MainLayout.js
@@ -0,0 +1,82 @@
+import React, {useContext, useEffect, useState} from 'react';
+import {AppBar, Toolbar, Typography, Button, Box, Container, Grid} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import {Link} from 'react-router-dom';
+// import {AlertSnack} from '../pages/component';
+// import {AuthContext} from '../context/AuthContext';
+import slogo from "../images/slogo.png"
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1
+    },
+    menuButton: {
+        marginRight: theme.spacing(2)
+    },
+    title: {
+        flexGrow: 1
+    },
+    headerBar: {
+        display: 'flex',
+        marginTop: '6rem'
+    },
+    main: {
+        display: 'flex',
+        minHeight: window.innerHeight
+    }
+}));
+
+const MainLayout = ({children}) => {
+    const classes = useStyles();
+    // const { alertMessage: authMessage, currentUser, logout} = useContext(AuthContext);
+
+    // const messageData = () => {
+    //     if (authMessage) {
+    //         return authMessage;
+    //     }
+    // };
+
+    const loginUserData = sessionStorage.getItem('user');
+    const [loginUser, SetLoginUser] = useState(false);
+
+    useEffect(() => {
+        if (loginUserData !== null) {
+            SetLoginUser(true);
+        } else {
+            SetLoginUser(false);
+        }
+    }, [loginUserData]);
+
+    return (
+        <container>
+            <Grid item >
+                <Grid container item  >
+                    <Grid container item style={{padding:10,margin:20,height:80,border: "1px solid black"}}>
+                        <Grid item md={5}>
+                            <Grid item md={4} style={{justifyContent:"center"}}>
+                                <Typography align="center" style={{fontFamily:"papyrus",fontWeight:"bold", padding:10,margin:2, border: "1px solid black"}} >Home</Typography>
+                            </Grid>
+
+                        </Grid>
+                        <Grid item style={{height:60}} md={2}>
+                            <img style={{padding:5,margin:2,maxWidth: "80%",opacity:0.8}}  src={slogo}></img>
+                        </Grid>
+                        <Grid container item md={5}>
+                            <Grid item md={4} style={{justifyContent:"center"}}>
+                                <Typography align="center" style={{fontFamily:"papyrus",fontWeight:"bold",padding:10,margin:2, border: "1px solid black"}} >Courses</Typography>
+                            </Grid>
+                            <Grid item md={4} style={{justifyContent:"center"}}>
+                                <Typography align="center" style={{fontFamily:"papyrus",fontWeight:"bold",padding:10,margin:2, border: "1px solid black"}} >Login</Typography>
+                            </Grid>
+                            <Grid item md={4} style={{justifyContent:"center"}}>
+                                <Typography align="center" style={{fontFamily:"papyrus",fontWeight:"bold",padding:10,margin:2, border: "1px solid black"}} >SignUp</Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </container>
+    );
+};
+
+export default MainLayout;
